f65807c926ab55103ae974bcfff0da381de910a4
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-05 10:26:05 -0500

    day 3 commit

--- 03/main.py
+++ 03/main.py
@@ -3,7 +3,7 @@ import typing
 _ALPHABET = {"0", "1"}  # TODO: explain
 
 
-def calculate_optima(line_indices_by_glyph, expected_count, tie_breaker=None):
+def calculate_optima(line_indices_by_glyph, expected_count=None, tie_breaker=None):
     """
     TODO EXPLAIN
 
@@ -44,28 +44,66 @@ def calculate_optima(line_indices_by_glyph, expected_count, tie_breaker=None):
         i += 1
 
     # Ensure that the total digit count across all digits is that which is expected
-    if total_count != expected_count:
+    if expected_count and total_count != expected_count:
         raise ValueError(f"Expecting a count of {expected_count}, but tallied a total of {total_count}")
 
     # Ensure that the optima are authoritative (there isn't a tie for 1st or a tie for last)
-    maxima_count = minima_count = 0
-    for _, count in digits_and_counts:
-        if count == max_count:
-            maxima_count += 1
-        if count == min_count:
-            minima_count += 1
-    if maxima_count != 1:
-        raise \
-            ValueError(
-                f"There are {maxima_count} maxima, each having {max_count} presentations; expecting 1 such maximum"
-            )
-    elif minima_count != 1:
-        raise \
-            ValueError(
-                f"There are {minima_count} minima, each having {min_count} presentations; expecting 1 such minimum"
-            )
+    if not tie_breaker:
+        maxima_count = minima_count = 0
+        for _, count in digits_and_counts:
+            if count == max_count:
+                maxima_count += 1
+            if count == min_count:
+                minima_count += 1
+        if maxima_count != 1:
+            raise \
+                ValueError(
+                    f"There are {maxima_count} maxima, each having {max_count} presentations; expecting 1 such maximum"
+                )
+        elif minima_count != 1:
+            raise \
+                ValueError(
+                    f"There are {minima_count} minima, each having {min_count} presentations; expecting 1 such minimum"
+                )
 
     return min_digit, max_digit
+
+
+def filter_(lines_by_glyph_by_pos, prefer_max):
+    """
+    TODO EXPLAIN
+
+    :param list[dict[str,set[int]]] lines_by_glyph_by_pos:
+    :param bool prefer_max:
+
+    :return:
+    :rtype: int
+    """
+
+    # Resolve the total line-count; should be the sum of the sizes of any of the glyph sets for a position
+    total_line_count = sum(len(lines) for glyph, lines in lines_by_glyph_by_pos[0].items())
+    candidates = set(range(total_line_count))
+    tie_breaker = "1" if prefer_max else "0"
+
+    for i, lines_by_glyph in enumerate(lines_by_glyph_by_pos):
+        if len(candidates) == 1:
+            break
+        next_lines_by_glyph = lines_by_glyph_by_pos[i]
+        filtered_lines_by_glyph = {
+            glyph: candidates.intersection(lines) for glyph, lines in next_lines_by_glyph.items()
+        }
+
+        min_digit, max_digit = calculate_optima(filtered_lines_by_glyph, tie_breaker=tie_breaker)
+        selector = max_digit if prefer_max else min_digit
+        candidates = filtered_lines_by_glyph[selector]
+
+    if len(candidates) != 1:
+        raise \
+            ValueError(
+                f"Expecting a decisive candidate, but remaining candidate set contains {len(candidates)} elements"
+            )
+    result_index = next(iter(candidates))
+    return result_index
 
 
 def evaluate_expression(digits):
@@ -91,7 +129,9 @@ def load_values(path="input.txt"):
     :return: 2-tuple giving...
       1. (int) GAMMA RATE
       2. (int) EPSILON RATE
-    :rtype: (int, int)
+      3. (int) O2 RATING
+      4. (int) C02 RATING
+    :rtype: (int, int, int, int)
     """
 
     with open(path) as infile:
@@ -137,8 +177,6 @@ def load_values(path="input.txt"):
     # All the lines' glyphs have been counted; time to find the winning glyph per line
     gamma_digits = []       # type: typing.List[str]
     epsilon_digits = []     # type: typing.List[str]
-    oxygen_rating_contribution_candidates = set(i for i in range(len(lines)))
-    c02_rating_contribution_candidates = set(i for i in range(len(lines)))
     for line_indices_by_glyph in counter_sequence:
         # The next gamma digit is that which is most-frequently found at this position
         epsilon_digit, gamma_digit = calculate_optima(line_indices_by_glyph, len(lines))
@@ -148,7 +186,10 @@ def load_values(path="input.txt"):
     gamma_value = evaluate_expression(gamma_digits)
     epsilon_value = evaluate_expression(epsilon_digits)
 
-    return gamma_value, epsilon_value
+    oxygen_rating = evaluate_expression(list(lines[filter_(counter_sequence, True)]))
+    c02_rating = evaluate_expression(list(lines[filter_(counter_sequence, False)]))
+
+    return gamma_value, epsilon_value, oxygen_rating, c02_rating
 
 
 def do():
@@ -158,9 +199,11 @@ def do():
     :return: None
     """
 
-    gamma_rate, epsilon_rate = load_values()
+    gamma_rate, epsilon_rate, o2_rating, c02_rating = load_values()
     rates_product = gamma_rate * epsilon_rate
+    ratings_product = o2_rating * c02_rating
     print(f"{rates_product}")
+    print(f"{ratings_product}")
 
 
 if __name__ == "__main__":
