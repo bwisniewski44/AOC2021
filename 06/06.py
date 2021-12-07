821e8fbc6f24d7d631656d7a84c17ea8f1c4860d
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-07 08:30:50 -0500

    Add a 'build rotator' function

--- 06/06.py
+++ 06/06.py
@@ -5,6 +5,9 @@ This script is part of a solution set devised to complete the 'Advent of Code' p
 https://adventofcode.com/2021
 """
 
+from collections import deque
+import typing
+
 
 def load_input(path="input.txt"):
     """
@@ -13,6 +16,52 @@ def load_input(path="input.txt"):
     :param str path: path to the file to read as input to this script
 
     :return:
+    :rtype: list[int]
+    """
+
+    with open(path) as infile:
+        lines = [str.strip(line) for line in infile.readlines()]
+    vector = [int(element) for element in str.split(lines[0], ",")]
+    return vector
+
+
+def build_rotator(values, theoretical_max=7):
+    """
+    TODO EXPLAIN
+
+    :param Iterable[int] values:
+    :param int theoretical_max:
+
+    :return:
+    :rtype: deque[int]
+    """
+
+    # Group the members by value
+    tally_by_value = {}  # type: typing.Dict[int,int]
+    for element in values:
+        old_tally = tally_by_value.get(element, 0)
+        tally_by_value[element] = old_tally + 1
+
+    # Ensure every possible entry is represented, introducing a 0-tally for those absent
+    for element in range(theoretical_max+1):
+        if element not in tally_by_value:
+            tally_by_value[element] = 0
+
+    # Sort the groups by order of ascending value (by group value... NOT by tally)
+    sorted_tallies = deque(tally_by_value[value] for value in sorted(tally_by_value.keys()))
+    return sorted_tallies
+
+
+def run_simulation(days, tallies, maturation_period=0):
+    """
+    TODO EXPLAIN
+
+    :param int days:
+    :param deque[int] tallies:
+    :param int maturation_period:
+
+    :return:
+    :rtype: int
     """
     pass
 
@@ -23,7 +72,9 @@ def main():
 
     :return: None
     """
-    pass
+
+    vector = load_input()
+    rotator = build_rotator(vector)
 
 
 if __name__ == "__main__":
