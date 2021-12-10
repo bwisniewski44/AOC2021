fbea667401e2334156742b68e833f19ca212c785
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-10 08:07:00 -0500

    day 9 done

--- 09/09.py
+++ 09/09.py
@@ -185,6 +185,92 @@ def find_minima(grid):
     return minima
 
 
+def expand_basin(coordinate, members, grid):
+    """
+    TODO EXPLAIN
+
+    :param (int,int) coordinate:
+    :param set[(int,int)] members:
+    :param Grid grid:
+
+    :return: None
+    """
+
+    if grid[coordinate] != 9:  # TODO: hardcode?
+        if coordinate not in members:
+            members.add(coordinate)
+
+            for direction in {Grid.UP, Grid.DOWN, Grid.LEFT, Grid.RIGHT}:
+                try:
+                    next_coordinate = grid.move(coordinate, direction)
+                except IndexError:
+                    pass
+                else:
+                    expand_basin(next_coordinate, members, grid)
+
+
+def get_basin_number_by_coordinate(grid):
+    """
+    TODO EXPLAIN
+
+    :param grid:
+
+    :return:
+    :rtype: dict[(int,int), int]
+    """
+
+    basin_number_by_coordinate = {}
+
+    i = 0
+    basins = 0
+    while i < grid.height:
+        j = 0
+        while j < grid.width:
+            coordinate = (i, j)
+            if grid[coordinate] != 9 and coordinate not in basin_number_by_coordinate:
+                basin_members = set()   # type: typing.Set[typing.Tuple[int,int]]
+                expand_basin(coordinate, basin_members, grid)
+
+                for member in basin_members:
+                    basin_number_by_coordinate[member] = basins
+                basins += 1
+
+            j += 1
+        i += 1
+
+    return basin_number_by_coordinate
+
+
+def get_basin_geography(grid):
+    """
+    TODO EXPLAIN
+
+    :param Grid grid:
+
+    :return:
+    :rtype: list[set[(int,int)]]
+    """
+
+    basin_number_by_coordinate = get_basin_number_by_coordinate(grid)
+
+    basins = []             # type: typing.List[typing.Set[typing.Tuple[int,int]]]
+    index_by_number = {}    # type: typing.Dict[int,int]
+    for coordinate, basin_number in basin_number_by_coordinate.items():
+        # Resolve the index at which this basin's members are being kept
+        if basin_number in index_by_number:
+            index = index_by_number[basin_number]
+        else:
+            index = len(basins)
+            index_by_number[basin_number] = index
+            basins.append(set())
+
+        # Add this coordinate to the basin's members
+        basin_members = basins[index]
+        basin_members.add(coordinate)
+
+    return basins
+
+
 def main():
     """
     TODO EXPLAIN
@@ -194,6 +280,14 @@ def main():
     grid = load_input()
     minima = find_minima(grid)
     print(sum(assess_risk(grid[coordinate]) for coordinate in minima))
+
+    basins = get_basin_geography(grid)
+    ordered_basins = sorted(basins, key=lambda basin: len(basin), reverse=True)
+    qualifying_basins = ordered_basins[:3]
+    size_product = 1
+    for basin in qualifying_basins:
+        size_product *= len(basin)
+    print(size_product)
 
 
 if __name__ == "__main__":
