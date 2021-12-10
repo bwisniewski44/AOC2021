51e67a7525dd77c27f2988c80092733c562d1a77
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-10 17:10:38 -0500

    day 10, part 1

--- 10/10.py
+++ 10/10.py
@@ -5,6 +5,29 @@ This script is part of a solution set devised to complete the 'Advent of Code' p
 https://adventofcode.com/2021
 """
 
+import typing
+
+EXPECTED_OPENER_AND_SCORE_BY_CLOSER = {
+    ")": ("(", 3),
+    "]": ("[", 57),
+    "}": ("{", 1197),
+    ">": ("<", 25137),
+}
+
+
+class Node(object):
+    """
+    TODO EXPLAIN
+    """
+
+    def __init__(self, opener):
+        """
+        :param str opener:
+        """
+        self.opener = opener
+        self.closer = None
+        self.children = []  # type: typing.List[Node]
+
 
 def load_input(path="input.txt"):
     """
@@ -13,8 +36,40 @@ def load_input(path="input.txt"):
     :param str path: path to the file to read as input to this script
 
     :return:
+    :rtype: list[str]
     """
-    pass
+
+    with open(path) as infile:
+        lines = [str.strip(line) for line in infile.readlines()]
+    return lines
+
+
+def scan(line):
+    """
+    TODO EXPLAIN
+
+    :param str line:
+
+    :return:
+    :rtype: int | None
+    """
+
+    unfinished_nodes = []   # type: typing.List[Node]
+    for ch in line:
+        if ch not in EXPECTED_OPENER_AND_SCORE_BY_CLOSER:
+            node = Node(ch)
+            unfinished_nodes.append(node)
+        else:
+            expected_opener, score = EXPECTED_OPENER_AND_SCORE_BY_CLOSER[ch]
+            node = unfinished_nodes.pop()
+            parent_node = unfinished_nodes[-1]
+            parent_node.children.append(node)
+
+            actual_opener = node.opener
+            if actual_opener != expected_opener:
+                return score
+
+    return None
 
 
 def main():
@@ -23,7 +78,14 @@ def main():
 
     :return: None
     """
-    pass
+    lines = load_input()
+
+    score = 0
+    for line in lines:
+        syntax_error_value = scan(line)
+        if syntax_error_value is not None:
+            score += syntax_error_value
+    print(score)
 
 
 if __name__ == "__main__":
