48e334ec487f9d2ef6dae5bb3db8a77ac741ed47
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-12 18:27:32 -0500

    Part 1, day 12

--- 12/12.py
+++ 12/12.py
@@ -5,7 +5,6 @@ This script is part of a solution set devised to complete the 'Advent of Code' p
 https://adventofcode.com/2021
 """
 
-from structures import KeySet
 import typing
 
 KEY_GOAL = "end"
@@ -13,23 +12,46 @@ KEY_START = "start"
 SEPARATOR = "-"
 
 
-def add_destination(destinations_by_origin, origin, destination):
+class NodeInfo(object):
+    """
+    TODO EXPLAIN
+    """
+
+    def __init__(self, name):
+        """
+        :param str name: TODO EXPLAIN
+        """
+        self.name = name
+        if not name.isalpha():
+            raise ValueError(f"Expecting all-letters for node name, but encountered {repr(name)}")
+        self.visitation_limit = None if name.isupper() else 1
+        self.visits = 0
+        self.neighbors = set()  # type: typing.Set[str]
+
+    @property
+    def accepting_visits(self):
+        if self.visitation_limit and self.visits >= self.visitation_limit:
+            return False
+        return True
+
+
+def add_destination(all_nodes, origin, destination):
     """
     TODO EXPLAIN
 
-    :param dict[str,set[str]] destinations_by_origin:
+    :param dict[str,NodeInfo] all_nodes:
     :param str origin:
     :param str destination:
 
     :return: None
     """
 
-    if origin in destinations_by_origin:
-        destinations = destinations_by_origin[origin]
+    if origin in all_nodes:
+        node_info = all_nodes[origin]
     else:
-        destinations = set()
-        destinations_by_origin[origin] = destinations
-    destinations.add(destination)
+        node_info = NodeInfo(origin)
+        all_nodes[origin] = node_info
+    node_info.neighbors.add(destination)
 
 
 def load_input(path="input.txt"):
@@ -39,18 +61,55 @@ def load_input(path="input.txt"):
     :param str path: path to the file to read as input to this script
 
     :return:
-    :rtype: dict[str,set[str]]
+    :rtype: dict[str,NodeInfo]
     """
 
-    destinations_by_origin = {}  # type: typing.Dict[str,typing.Set[str]]
+    info_by_node = {}  # type: typing.Dict[str,NodeInfo]
 
     with open(path) as infile:
         for line in infile.readlines():
             half_a, half_b = map(str.strip, line.split(SEPARATOR))  # type: str, str
-            add_destination(destinations_by_origin, half_a, half_b)
-            add_destination(destinations_by_origin, half_b, half_a)
+            add_destination(info_by_node, half_a, half_b)
+            add_destination(info_by_node, half_b, half_a)
 
-    return destinations_by_origin
+    return info_by_node
+
+
+def add_paths_from(node, info_by_node, all_paths, current_path=None):
+    """
+    TODO EXPLAIN
+
+    :param str node:
+    :param dict[str,NodeInfo] info_by_node:
+    :param list[list[str]] all_paths:
+    :param list[str] | None current_path:
+
+    :return: None
+    """
+
+    # Before we do anything, a bit of housekeeping: if the current path is NONE, this must be the root call of this
+    # recursive function... initialize the current path to an empty sequence of visited nodes
+    if current_path is None:
+        current_path = []
+
+    # We've arrived at a new node! Increment its visitation counter
+    current_path.append(node)
+    node_info = info_by_node[node]
+    node_info.visits += 1
+
+    if node == KEY_GOAL:
+        all_paths.append(list(current_path))
+    else:
+        # This isn't the goal node; need to explore further nodes to be able to reach the goal
+        for destination in node_info.neighbors:
+            # Skip the destination if visiting it would violate its visitation limit
+            if info_by_node[destination].accepting_visits:
+                add_paths_from(destination, info_by_node, all_paths, current_path)
+
+    # All paths stemming from the 'current path' have been added; let's pop the node off the 'current path' as if we
+    # never arrived here... the caller can then explore other routes to GOAL
+    current_path.pop()
+    node_info.visits -= 1
 
 
 def main():
@@ -59,7 +118,12 @@ def main():
 
     :return: None
     """
-    destinations_by_origin = load_input()
+    info_by_node = load_input()
+
+    all_paths = []  # type: typing.List[typing.List[str]]
+    add_paths_from(KEY_START, info_by_node, all_paths)
+
+    print(len(all_paths))
 
 
 if __name__ == "__main__":
