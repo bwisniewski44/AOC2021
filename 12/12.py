cd1bd211bab4b166ccefea2451dfe0a88f61ed15
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-12 09:24:06 -0500

    Initial commit day 12

--- 12/12.py
+++ 12/12.py
@@ -5,6 +5,32 @@ This script is part of a solution set devised to complete the 'Advent of Code' p
 https://adventofcode.com/2021
 """
 
+from structures import KeySet
+import typing
+
+KEY_GOAL = "end"
+KEY_START = "start"
+SEPARATOR = "-"
+
+
+def add_destination(destinations_by_origin, origin, destination):
+    """
+    TODO EXPLAIN
+
+    :param dict[str,set[str]] destinations_by_origin:
+    :param str origin:
+    :param str destination:
+
+    :return: None
+    """
+
+    if origin in destinations_by_origin:
+        destinations = destinations_by_origin[origin]
+    else:
+        destinations = set()
+        destinations_by_origin[origin] = destinations
+    destinations.add(destination)
+
 
 def load_input(path="input.txt"):
     """
@@ -13,8 +39,18 @@ def load_input(path="input.txt"):
     :param str path: path to the file to read as input to this script
 
     :return:
+    :rtype: dict[str,set[str]]
     """
-    pass
+
+    destinations_by_origin = {}  # type: typing.Dict[str,typing.Set[str]]
+
+    with open(path) as infile:
+        for line in infile.readlines():
+            half_a, half_b = map(str.strip, line.split(SEPARATOR))  # type: str, str
+            add_destination(destinations_by_origin, half_a, half_b)
+            add_destination(destinations_by_origin, half_b, half_a)
+
+    return destinations_by_origin
 
 
 def main():
@@ -23,7 +59,7 @@ def main():
 
     :return: None
     """
-    pass
+    destinations_by_origin = load_input()
 
 
 if __name__ == "__main__":
