a528ff3a4b7b3d99e4b111274a84e5f53c6d2420
bwisniewski <bwisniewski@vargomail.com>
2021-12-14 08:29:31 -0500

    Day 14: add function to load input; define input

--- 14/14.py
+++ 14/14.py
@@ -5,6 +5,10 @@ This script is part of a solution set devised to complete the 'Advent of Code' p
 https://adventofcode.com/2021
 """
 
+import typing
+
+SEPARATOR = "->"
+
 
 def load_input(path="input.txt"):
     """
@@ -12,9 +16,33 @@ def load_input(path="input.txt"):
 
     :param str path: path to the file to read as input to this script
 
-    :return:
+    :return: productions by pair
+    :rtype: (list[str], dict[(str,str), str])
     """
-    pass
+    with open(path) as infile:
+        lines = [str.strip(line) for line in infile.readlines()]
+
+    base = list(lines[0])
+
+    productions_by_pair = {}  # type: typing.Dict[typing.Tuple[str,str], str]
+    i = 2
+    while i < len(lines):
+        line = lines[i]
+        try:
+            pair_expression, production = map(str.strip, line.split(SEPARATOR))
+        except ValueError:
+            raise ValueError(f"Expecting a single separator ({SEPARATOR}) in line {repr(line)}")
+
+        pair = tuple(pair_expression)
+        if len(pair) != 2:
+            raise ValueError(f"Expecting two elements in generating sequence; got {len(pair)}")
+        elif len(production) != 1:
+            raise ValueError(f"Expecting a single element in generated production; got {len(production)}")
+        productions_by_pair[pair] = production
+
+        i += 1
+
+    return base, productions_by_pair
 
 
 def main():
@@ -23,7 +51,7 @@ def main():
 
     :return: None
     """
-    pass
+    load_input()
 
 
 if __name__ == "__main__":
