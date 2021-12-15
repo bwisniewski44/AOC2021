f0ceed2b72264f1495f850422a179d01e5ecd01e
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-15 11:39:45 -0500

    Populate a results structure; in the future, can hold more than just the score associated with optimal path

--- 15/15.py
+++ 15/15.py
@@ -11,24 +11,30 @@ from structures import Grid
 DECLINED = 0
 
 
-def populate_optimum(grid, goal, current_optimum, coordinate=(0, 0), current_score=0):
+class Results:
+    def __init__(self, max_score):
+        """
+        :param int max_score:
+        """
+        self.score = max_score  # type: int
+
+
+def populate_optimum(results, grid, goal, coordinate=(0, 0), current_score=0):
     """
     Gives the score associated with the optimal path from the given coordinate of the board to the goal coordinate.
 
+    :param Results results:
     :param Grid grid: board over which to traverse
     :param (int,int) goal: position marking the end of the path
-    :param int current_optimum:
     :param (int,int) coordinate: latest-entered position; this shall either be the goal position (base case) or that
       from which to move towards the goal position
     :param int current_score: score already accumulated by entering the given coordinate
 
-    :return: score associated with most-optimal path from the given coordinate
-    :rtype: int
+    :return: None
     """
 
     # BASE CASE: the optimum is already better than this competing score - stop trying
-    if current_optimum <= current_score:
-        optimum = current_optimum
+    if results.score <= current_score:
         global DECLINED
         DECLINED += 1
         if DECLINED % 10000 == 0:
@@ -36,13 +42,12 @@ def populate_optimum(grid, goal, current_optimum, coordinate=(0, 0), current_sco
 
     # BASE CASE: we're at the goal position... this must be the new optimum
     elif coordinate == goal:
-        optimum = current_score
-        print(f"Found optimum {optimum}")
+        results.score = current_score
+        print(f"Found optimum {results.score}")
 
     # RECURSIVE CASE: the current score is still better than the latest optimum, but we've yet to reach the necessary
     # position; let's continue accumulating onto this competing score as we head towards the goal
     else:
-        optimum = current_optimum
         for direction in {Grid.DOWN, Grid.RIGHT}:
             # Resolve the coordinate to which to move
             try:
@@ -50,12 +55,9 @@ def populate_optimum(grid, goal, current_optimum, coordinate=(0, 0), current_sco
             except IndexError:
                 continue  # oh well, must be at edge of grid
             destination_value = grid[next_coordinate]
-            optimum = \
-                populate_optimum(
-                    grid, goal, optimum, coordinate=next_coordinate, current_score=current_score+destination_value
-                )
-
-    return optimum
+            populate_optimum(
+                results, grid, goal, coordinate=next_coordinate, current_score=current_score+destination_value
+            )
 
 
 def main():
@@ -67,8 +69,9 @@ def main():
     board = Grid.load("input.txt")
 
     goal = (board.height-1, board.width-1)
-    result = populate_optimum(board, goal, board.width * board.height * 9)
-    print(result)
+    results = Results(board.width * board.height * 9)
+    populate_optimum(results, board, goal)
+    print(results.score)
 
 
 if __name__ == "__main__":
