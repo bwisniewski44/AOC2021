19b7aa56459c556dbf68eddec2ef89adb0e51245
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-16 10:17:36 -0500

    Use a priority queue instead of iterating through lists

--- 15/15.py
+++ 15/15.py
@@ -7,7 +7,7 @@ https://adventofcode.com/2021
 
 import typing
 from collections import deque
-from structures import Grid
+from structures import Grid, PriorityQueue
 
 
 class Node:
@@ -22,6 +22,17 @@ class Node:
         self.g = 0
         self.f = 0
         self.h = 0
+
+    def __lt__(self, other):
+        """
+        TODO EXPLAIN
+
+        :param Node other:
+
+        :return:
+        :rtype: bool
+        """
+        return self.f < other.f
 
     def __eq__(self, other):
         """
@@ -67,36 +78,39 @@ def search(grid, start_coordinates, goal_coordinates):
     :param (int, int) goal_coordinates:
 
     :return:
-    :rtype: list[(int,int)]
+    :rtype: list[(int,int)] | None
     """
 
     # Initialize the 'yet-to-visit' and 'visited' structures
-    frontier = []   # type: typing.List[Node]
+    frontier = PriorityQueue()
+    population_by_coordinate = {}  # type: typing.Dict[typing.Tuple[int,int], int]
     visited = set()     # type: typing.Set[typing.Tuple[int,int]]
 
     # Introduce the start node to the frontier
     start_node = Node(start_coordinates)
-    frontier.append(start_node)  # TODO: maybe faster as heap/deque?
+    frontier.push(start_node)  # TODO: maybe faster as heap/deque?
+    population_by_coordinate[start_node.position] = 1
 
     # While there are nodes to search...
+    path = None  # type: typing.Optional[typing.List[typing.Tuple[int,int]]]
     while len(frontier) > 0:
 
         # Search for the most-optimal node; we'll select the leading node and iterate through all others to find that
         # with the best 'f'
-        optimal_node = frontier[0]
-        optimal_index = 0
-        for frontier_index, frontier_element in enumerate(frontier):
-            if frontier_element.f < optimal_node.f:
-                optimal_node = frontier_element
-                optimal_index = frontier_index
+        optimal_node = frontier.pop()   # type: Node
+        updated_population = population_by_coordinate[optimal_node.position] - 1
+        if updated_population > 0:
+            population_by_coordinate[optimal_node.position] = updated_population
+        else:
+            population_by_coordinate.pop(optimal_node.position)
 
         # If the node is the goal node, then we can stop
         if optimal_node.position == goal_coordinates:
-            return resolve_path(optimal_node)
+            path = resolve_path(optimal_node)
+            break
 
         # We're still here, so we still have progress to make towards goal; begin by transferring the node out of the
         # 'yet-to-visit' list and into the 'visited' list
-        frontier.pop(optimal_index)
         visited.add(optimal_node.position)
 
         # Find the children node to which we may travel
@@ -120,9 +134,21 @@ def search(grid, start_coordinates, goal_coordinates):
             destination.f = destination.g + destination.h
 
             # Introduce this to the 'yet-to-visit' list if the node isn't already there OR if this one has a lower 'f'
-            if any(entrant == destination and entrant.f < destination.f for entrant in frontier):
-                continue
-            frontier.append(destination)
+            if destination.position not in population_by_coordinate:
+                introduce = True
+            else:
+                # Introduce only if this destination has optimal 'f'
+                introduce = True
+                for item in frontier._values:
+                    if item.position == destination.position and item.f < destination.f:
+                        introduce = False
+                        break
+            if introduce:
+                frontier.push(destination)
+                population_by_coordinate[destination.position] = \
+                    population_by_coordinate.get(destination.position, 0) + 1
+
+    return path
 
 
 def main():
