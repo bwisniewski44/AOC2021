7e897dbb3545d26241860dc662e707846db0be47
Brian Wisniewski <brianwisniewski44@gmail.com>
2021-12-19 19:47:45 -0500

    Transpose result before sending to file

--- 14/14.py
+++ 14/14.py
@@ -17,29 +17,44 @@ def load_input(path="input.txt"):
     :param str path: path to the file to read as input to this script
 
     :return: productions by pair
-    :rtype: (list[str], dict[(str,str), str])
+    :rtype: (str, dict[(str,str), (str,str)])
     """
     with open(path) as infile:
         lines = [str.strip(line) for line in infile.readlines()]
 
+    # The first line gives the initial sequence
     base = list(lines[0])
 
-    productions_by_pair = {}  # type: typing.Dict[typing.Tuple[str,str], str]
+    # A blank line separates the initial from those defining the productions; starting after those first two lines,
+    # iterate over those defining the productions
+    productions_by_pair = \
+        {}  # type: typing.Dict[typing.Tuple[str,str], typing.Tuple[typing.Tuple[str,str], typing.Tuple[str,str]]]
     i = 2
     while i < len(lines):
+        # Line should be a format like "AB -> X"; divide out the "AB" and "X" portions
         line = lines[i]
         try:
-            pair_expression, production = map(str.strip, line.split(SEPARATOR))
+            pair_expression, produced_element = map(str.strip, line.split(SEPARATOR))  # type: str, str
         except ValueError:
             raise ValueError(f"Expecting a single separator ({SEPARATOR}) in line {repr(line)}")
 
+        # The first "AB" portion should be a pair of letters; that's our pair key... make sure it's two letters and the
+        # production is just one!
         pair = tuple(pair_expression)
         if len(pair) != 2:
             raise ValueError(f"Expecting two elements in generating sequence; got {len(pair)}")
-        elif len(production) != 1:
-            raise ValueError(f"Expecting a single element in generated production; got {len(production)}")
-        productions_by_pair[pair] = production
+        elif len(produced_element) != 1:
+            raise ValueError(f"Expecting a single element in generated production; got {len(produced_element)}")
 
+        # Inserting an element between AB to yield AXB has the following consequences: there is one fewer AB pair,
+        # replaced by two new pairs - AX, XB
+        productions = (
+            (pair[0], produced_element),    # here's the 1st produced pair (AX)
+            (produced_element, pair[1])     # here's the 2nd produced pair (XB)
+        )
+        productions_by_pair[pair] = productions
+
+        # Advance to parse the next line
         i += 1
 
     return base, productions_by_pair
